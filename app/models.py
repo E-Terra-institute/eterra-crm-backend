3d269629f00e395266610c836cdfa15a0e65ab1c
E-Terra-institute <institute@e-terra.com.ua>
2025-07-16 02:41:50 +0300

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from app.database import Base
+
+class Request(Base):
+    __tablename__ = "requests"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=True)
+    message = Column(String, nullable=False)
+    platform = Column(String, default="telegram")
+    created_at = Column(DateTime, default=datetime.utcnow)
